f2a9aacee6463100e6e5945b8bbdbd5255ffce08
David Knight <davidknight42@icloud.com>
2019-08-27 16:44:11 +0100

    Create Otest.php

---
+++ Otest.php
@@ -0,0 +1,7 @@
+<time class="p2 singleCatDate__time" datetime=" 
+  <?php echo get_the_date(); ?> 
+  <?php echo get_the_time(); ?>" title="Published 
+  <?php echo get_the_date(); ?> 
+  <?php echo get_the_time(); ?>"> 
+        <?php echo get_the_date(); ?>
+</time>
